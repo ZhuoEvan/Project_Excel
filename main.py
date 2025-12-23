c8aeef15583547c8c705e47ce3b2d36f9260f5d7
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2025-12-23 17:13:04 -0600

    started main as empty

--- main.py
+++ main.py
@@ -10,29 +10,18 @@
 
 
 # Python Library ------------------ #
-import tkinter as tk
 import pandas as pd
 
 
 # ================================= #
-# Tkinter Construct                 #
+# Main Function                     #
 # ================================= #
 #Initialize Application
-def main(master):
-    #Initial Settings
-    root.title("Project Excel")
-    root.geometry("1440x900")
-
-    #Build Main Menu
-    WIN.MainWindow(master)
-
-    #Start Application
-    root.mainloop()
+def main():
+    return 0
     
-#Build Application
 if __name__ == "__main__":
-    root = tk.Tk()
-    main(root)
+    main()
     
 
 #Application Ideas
